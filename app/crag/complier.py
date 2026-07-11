81b8f3430bd403f800f03c7f710f3745014b7fb3
Adexandria <adeolaaderibigbe09@gmail.com>
2026-07-11 15:43:37 +0200

    Set up lang graph nodes and edges

---
+++ app/crag/complier.py
@@ -0,0 +1,32 @@
+from app.crag.nodes import generate, retrieval, transform_query, aggregate_results,grade_structure,retrieve_transformed, decide_to_generate,grade_answer, decide_to_generate
+from app.crag.state import GraphState
+from langgraph.graph import END, StateGraph, START
+
+workflow  = StateGraph(GraphState)
+
+workflow.add_node("retrieve", retrieval)
+workflow.add_node("grade_structure", grade_structure)
+workflow.add_node("transform_query", transform_query)
+workflow.add_node("generate", generate)
+workflow.add_node("retrieve_transformed", retrieve_transformed)
+workflow.add_node("aggregate_results", aggregate_results)
+workflow.add_node("grade_generation", grade_answer)
+
+## Build graph edges
+workflow.add_edge(START, "retrieve")
+workflow.add_edge("retrieve", "grade_structure")
+workflow.add_conditional_edges(
+    "grade_structure",
+    decide_to_generate,
+    {
+        "aggregate_results": "aggregate_results",
+        "transform_query": "transform_query"
+    },
+)
+
+workflow.add_edge("transform_query", "retrieve_transformed")
+workflow.add_edge("retrieve_transformed", "grade_structure")
+workflow.add_edge("aggregate_results", "generate")
+workflow.add_edge("generate", "grade_generation")
+workflow.add_edge("grade_generation", END)
+
