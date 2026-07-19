37607cc113c228203bb4fb6761f790346479b040
Adexandria <adeolaaderibigbe09@gmail.com>
2026-07-19 17:24:51 +0200

    Update workflow to support non corrective workflow

--- app/crag/graph.py
+++ app/crag/graph.py
@@ -2,40 +2,80 @@ from app.crag.nodes import generate, retrieval, transform_query, aggregate_resul
 from app.crag.state import GraphState
 from langgraph.graph import END, StateGraph, START
 
-workflow  = StateGraph(GraphState)
 
-workflow.add_node("retrieve", retrieval)
-workflow.add_node("grade_structure", grade_structure)
-workflow.add_node("transform_query", transform_query)
-workflow.add_node("generate", generate)
-workflow.add_node("retrieve_transformed", retrieve_transformed)
-workflow.add_node("aggregate_results", aggregate_results)
-workflow.add_node("grade_generation", grade_answer)
 
-## Build graph edges
-workflow.add_edge(START, "retrieve")
-workflow.add_edge("retrieve", "grade_structure")
-workflow.add_conditional_edges(
+
+def corrective_workflow():
+    workflow  = StateGraph(GraphState)
+    workflow.add_node("retrieve", retrieval)
+    workflow.add_node("grade_structure", grade_structure)
+    workflow.add_node("transform_query", transform_query)
+    workflow.add_node("generate", generate)
+    workflow.add_node("retrieve_transformed", retrieve_transformed)
+    workflow.add_node("aggregate_results", aggregate_results)
+    workflow.add_node("grade_generation", grade_answer)
+
+    # Build graph edges
+    workflow.add_edge(START, "retrieve")
+    workflow.add_edge("retrieve", "grade_structure")
+
+    workflow.add_conditional_edges(
     "grade_structure",
     decide_to_generate,
     {
         "aggregate_results": "aggregate_results",
-        "transform_query": "transform_query"
+        "transform_query": "transform_query",
     },
-)
+    )
 
-workflow.add_edge("transform_query", "retrieve_transformed")
-workflow.add_edge("retrieve_transformed", "grade_structure")
-workflow.add_edge("aggregate_results", "generate")
-workflow.add_edge("generate", "grade_generation")
-workflow.add_conditional_edges(
+    workflow.add_edge("transform_query", "retrieve_transformed")
+    workflow.add_edge("retrieve_transformed", "grade_structure")
+    workflow.add_edge("aggregate_results", "generate")
+    workflow.add_edge("generate", "grade_generation")
+
+    workflow.add_conditional_edges(
     "grade_generation",
     decide_after_judging,
     {
         "END": END,
         "generate": "generate",
-        "transform_query": "transform_query"
-    }
-)
+        "transform_query": "transform_query",
+    },
+    )
 
-app = workflow.compile()
+    app = workflow.compile()
+    return app
+
+def base_workflow():
+    workflow  = StateGraph(GraphState)
+    workflow.add_node("retrieve", retrieval)
+    workflow.add_node("grade_structure", grade_structure)
+    workflow.add_node("transform_query", transform_query)
+    workflow.add_node("generate", generate)
+    workflow.add_node("retrieve_transformed", retrieve_transformed)
+    workflow.add_node("aggregate_results", aggregate_results)
+    workflow.add_node("grade_generation", grade_answer)
+
+    # Build graph edges
+    workflow.add_edge(START, "retrieve")
+    workflow.add_edge("retrieve", "grade_structure")
+
+    workflow.add_conditional_edges(
+    "grade_structure",
+    decide_to_generate,
+    {
+        "aggregate_results": "aggregate_results",
+        "transform_query": "transform_query",
+    },
+    )
+
+    workflow.add_edge("transform_query", "retrieve_transformed")
+    workflow.add_edge("retrieve_transformed", "grade_structure")
+    workflow.add_edge("aggregate_results", "generate")
+    workflow.add_edge("generate", "grade_generation")
+
+    workflow.add_edge("grade_generation", END)
+
+
+    app = workflow.compile()
+    return app
