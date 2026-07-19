b523b37042367f96b359959eea9d70e919e1be28
Adexandria <adeolaaderibigbe09@gmail.com>
2026-07-19 17:28:38 +0200

    Add corrective workflow

---
+++ app/crag/complier/corrective.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+
+from app.crag.graph import corrective_workflow
+
+from app.docs.extract_template import generate_documentation
+from app.docs.response import MIMETYPE
+
+from app.config import MEDIA_PATH
+
+inputs = {
+    "query": "Which attempt gave the best result?",
+    "experiment_id": "1",
+     "retry_count": 0
+}
+app = corrective_workflow()
+final_state = app.invoke(inputs)
+
+final_state = None
+for mode, out in app.stream(inputs, stream_mode=["updates", "values"]):
+    if mode == "updates":
+        for node, delta in out.items():
+            pprint(f"Node '{node}':"); pprint(delta, indent=2)
+    else:
+        final_state = out      
+
+
+# print("Generating documentation...")
+generate_documentation(final_state, MEDIA_PATH, MIMETYPE.HTML)
+
+
+
